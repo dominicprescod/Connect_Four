79503ab9948b90c10c5b64feb32e82a7b9f89b0b
Dominic Prescod <dominicprescod@gmail.com>
2016-04-08 07:09:50 -0400

    redesign

---
+++ js/newdesign.js
@@ -0,0 +1,29 @@
+$(function(){
+
+  var remove = [1,3,5,7,12,13,15];
+  var r = 0;
+
+$('td').each(function(i){
+  if (i === remove[r]) {
+    setTimeout(function(){
+      $('td').eq(i).css('background','red');
+    }, 500);
+    setTimeout(function(){
+      $('td').eq(i).css('background','black');
+    }, 1000);
+    setTimeout(function(){
+      $('td').eq(i).css('background','#3FC380');
+    }, 1500);
+    r++;
+  }
+});
+
+
+
+
+
+
+
+
+
+});
